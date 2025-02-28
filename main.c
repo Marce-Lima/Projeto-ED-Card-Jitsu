8dc0262dc62c5066b3f660512d72c772e2d86b9f
Marce-Lima <166259893+Marce-Lima@users.noreply.github.com>
2025-02-28 02:51:05 -0300

    correção main e lista

--- main.c
+++ main.c
@@ -1,33 +1,97 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include "lista.h"
+#include "game.h"
 
-#define MAX_HAND 5
+void read_rules();
+void show_hand(Hand *hand);
 
-int main()
-{
-    hand* H = (hand*)malloc(sizeof(hand));
-    card a;
+int main(){
 
-    card Cards[] = {{0,1},{0,2},{1,1},{1,2},{2,1}};
+	Player player1, player2;
+	player_ p1 = &player1;
+	player_ p2 = &player2;
+	char answer;
+	int round_ = 0;
+	
+	Card allcards[] = {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
+                    {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7},
+                    {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6}, {2, 7}}; // define todas as cartas disponiveis
+	
+	init_Pack(&p1->pack, allcards);
+	init_Pack(&p2->pack, allcards);
 
-    inicializar_hand(H);
+	//criar player
+	init_player(p1, 1);
+	init_player(p2, 2);
+	
+	//tela inicial
+	printf("\n CARD-JITSU \n\n");
 
-    for (int i = 0; i < MAX_HAND; i++)
-    {
-        Insert(H, Cards[i]);
-    }
+	//ler regras
+	printf("Ler as regras? (s/n)\n");
+	scanf("%c", &answer);
+	if(answer == 's'){
+		read_rules();
+	}else{
+		printf("Ah, vejo que você já tem o conhecimento necessário...\n");
+		printf("Então mostre do que é capaz!\n\n");
+	}
+
+	//jogo em si
+	while(1){
+
+		//anunciar round
+		round_++;
+		printf("\n---->>>< ROUND %d ><<<----\n", round_);
+
+		// tipo 0 = fogo
+		// tipo 1 = agua
+		// tipo 2 = gelo
+		
+        turn(p1);
+        turn(p2);
+        
+        check(p1, p2);
+
+		//condicoes de vitoria
+		if(win(p1)){
+			break;
+		}
+		if(win(p2)){
+			break;
+		}
+	
+	}
+
+	return 0;
+}
+
+void turn(player_ p){
     
-    H->iterador = H->sentinela->next;
-
-    for (int i = 0; i < MAX_HAND; i++)
-    {
-        a = H->iterador->valor;
-        printf("\n[ %d | %d ]\n", a.type, a.number);
-        H->iterador = H->iterador->next;
-    }
+    int answer_num;
     
-    Destruct(H);
+    printf("== Vez de p%d ==\n\n", p->id);
+	printf("(Não permita que seu adversário veja suas cartas! Pressione 'Enter' para ver suas cartas.)\n");
+	show_hand(p->hand);
 
-    return 0;
+		//escolha
+	scanf("%d", &answer_num);
+	p->choice = select_card(answer_num, p->hand, &p->pack);
+	printf("Sua escolha foi %d - %d\n", p->choice.type, p->choice.number);
+	
+	//system("clear");
+    
+    return;
+}
+
+void read_rules(){
+
+	printf("---REGRAS---\n\nOlá, pequeno gafanhoto!\n Bem-vindo ao Card-Jitsu, um e-sporte tradicional dos tempo remotos de Club Penguin.\n");
+	printf("O jogo consiste em rodadas de batalhas de cartas. Cada jogador terá uma mão com cinco cartas\ne seu pack de cartas para repor a mão ao longo da partida.\n");
+	printf("Cada carta possui um elemento (água, fogo, gelo) e um valor (de 1 até 7).\nEm cada rodada os jogadores devem escolher uma cartasem que seu adversário saiba qual foi.\n\n");
+	printf("As cartas escolhidas entrarão em disputa ganhando pela regra de prioridade:\n\t1. Elementos diferentes - água vence fogo, fogo vence gelo e gelo vence água(como em pedra, papel e tesoura).\n");
+	printf("\t2. Maior valor - caso os elementos das cartas escolhidas sejam iguais, ganhará a carta de maior valor.\n");
+	printf("\t3. Caso ambos sejam iguais, é um empate técnico e ninguém obtem vitória na rodada.\nO formato das cartas quando mostradas é '(x) - [elemento | valor]', sendo x o número para escolhê-la.\n");
+	printf("Ganha aquele que completar 3 vitórias de um mesmo elemento ou 1 vitória de cada elemento.\n\nAgora, vamos para a prática!\n\n");
 }
